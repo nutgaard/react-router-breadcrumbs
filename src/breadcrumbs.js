50173858e11b4d6e251088fa12b243c56e55dbfe
Jade Montagu <jade@fundapps.co>
2017-05-02 17:52:26 +0100

    Update to react 15.5

--- src/breadcrumbs.js
+++ src/breadcrumbs.js
@@ -1,4 +1,5 @@
-import React, { PropTypes as PT } from 'react';
+import React from 'react';
+import PT from 'prop-types';
 import { Link, formatPattern } from 'react-router';
 import * as RouterProps from './router-props';
 import { on, not, where, pluck, isEqualTo, join, lastOf } from './utils';
