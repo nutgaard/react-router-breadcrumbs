1467b338b4f2179f6565233f3c0a67efe02c03d5
Nicklas Utgaard <nutgaard@gmail.com>
2017-07-23 12:54:28 +0200

    Updates dependencies

--- test/.setup.js
+++ test/.setup.js
@@ -1,9 +1,11 @@
 import "babel-polyfill";
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('');
+const { document } = (new JSDOM('')).window;
+global.document = document;
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
     if (typeof global[property] === 'undefined') {
